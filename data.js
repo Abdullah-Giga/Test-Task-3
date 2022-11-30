e9e3f1aeb15421b983b291c6628a7e017871787f
Abdullah Shahzad <abdullah.shahzad@gigalabs.co>
2022-11-30 12:30:20 +0500

    Removed a bug

--- data.js
+++ data.js
@@ -1,91 +1,85 @@
-
-
-// Taking an array of objects of the AM time
+// This array tajes data <= 12.5pm and >= 9 am
 const objArr_am = [
   {
     start: 9,
-    end: 9.5,
+    end: 13,
   },
-  
-
+  {
+    start: 10,
+    end: 11,
+  },
 ];
 
-// Taking an array of objects of the PM time
+// This array takes data in 24 hour format i.e 1 pm is 13 pm 2pm is 14pm
 
 const objArr_pm = [
   {
-    start: 3,
-    end: 4,
+    start: 13,
+    end: 14,
   },
   {
     start: 12,
-    end: 1,
+    end: 13,
   },
   {
-    start: 3.5,
-    end: 5,
+    start: 15.5,
+    end: 17,
   },
   {
-    start: 5,
-    end: 6,
-  },
-  {
-    start: 7,
-    end: 8,
-  },
-  
-  {
-    start: 5.5,
-    end: 8,
-  },  
-  {
-    start: 1,
-    end: 3.5,
+    start: 15,
+    end: 19,
   },
 ];
 
-
-
-
 // Sorting both arrays individually wrt the time sequence
-objArr_am.sort(function(a, b){
+objArr_am.sort(function (a, b) {
   return a.start - b.start;
 });
-objArr_pm.sort(function(a, b){
+objArr_pm.sort(function (a, b) {
   return a.start - b.start;
 });
-
-console.log(objArr_am);
-console.log(objArr_pm);
 
 
 // making a combined array
-
 const objArr = objArr_am.concat(objArr_pm);
-console.log(objArr);
 
+
+// Functio to hande 24 hour format
+function time(start) {
+  if (start > 12) {
+    return start - 12;
+  } else {
+    return start;
+  }
+}
 
 // Makng a new div for every scheduled task
 const getDiv = (start) => {
   let div = `
     
     <div class="r-item-full" >
-    <span class="gray-text">${start}:00 - </span><b>Sample Item</b> <span class="green-text"> Sample Location</span>
+    <span class="gray-text">${time(
+      start
+    )}:00 - </span><b>Sample Item</b> <span class="green-text"> Sample Location</span>
     </div>`;
   return div;
 };
+
+// Making new dv to handle tie starting with .5 or :30 minute format
 
 const getDivHalf = (start) => {
   let div = `
     
     <div class="r-item-half">
-    <span class="gray-text">${Math.trunc(start)}:30 - </span><b>Sample Item</b> <span class="green-text"> Sample Location</span>
+    <span class="gray-text">${Math.trunc(
+      time(start)
+    )}:30 - </span><b>Sample Item</b> <span class="green-text"> Sample Location</span>
     </div>`;
   return div;
 };
 
 
-
+// Find if collision occurs between two divs
 function collision_helper(a, b) {
   const rect1 = a.getBoundingClientRect();
   const rect2 = b.getBoundingClientRect();
@@ -97,63 +91,48 @@ function collision_helper(a, b) {
   return isOverlapping;
 }
 
-
-
-
-
-
-
-
-
-
 // Checking the collision and making divs responsive
-function collision(){
-  
-  for(let i = 0; i < objArr.length ; i++){
+function collision() {
+  for (let i = 0; i < objArr.length; i++) {
     let j = 0;
 
-    while(j != i && j < objArr.length - 1){
-      if(collision_helper(document.getElementById(objArr[j].start) ,document.getElementById(objArr[i].start))){
-
-  
+    while (j != i && j < objArr.length - 1) {
+      if (
+        collision_helper(
+          document.getElementById(objArr[j].start),
+          document.getElementById(objArr[i].start)
+        )
+      ) {
         let parent = document.getElementById(objArr[j].start);
         let child = document.getElementById(objArr[i].start);
         parent.appendChild(child);
-        console.log("hello",child.id);
-        child.style.marginTop = `${(objArr[i].start - objArr[j].start)*120}px`;
+        console.log("hello", child.id);
+        child.style.marginTop = `${
+          (objArr[i].start - objArr[j].start) * 120
+        }px`;
       }
       j++;
     }
   }
-  }
+}
 
-
-
-
-
-// Rendering all the new task divs  
+// Rendering all the new task divs
 const render = () => {
-  for (let i = 1; i <= 12; i+=0.5) {
+  for (let i = 9; i <= 20; i += 0.5) {
+
     let j = document.getElementById(i);
 
-    for(let k = 0; k < objArr.length; k++){
-
-        if (j.id == objArr[k].start && !j.id.includes(".5")) {
-          j.innerHTML +=  getDiv(objArr[k].start);
-          j.style.height = `${(objArr[k].end - objArr[k].start) * 120}px`; 
-
-        }
-        else if(j.id == objArr[k].start && j.id.includes(".")){
-          j.innerHTML +=  getDivHalf(objArr[k].start);
-          j.style.height += `${((objArr[k].end - objArr[k].start) * 120)}px`;
-
-        }
+    for (let k = 0; k < objArr.length; k++) {
+      if (j.id == objArr[k].start && !j.id.includes(".5")) {
+        j.innerHTML += getDiv(objArr[k].start);
+        j.style.height = `${(objArr[k].end - objArr[k].start) * 120}px`;
+      } else if (j.id == objArr[k].start && j.id.includes(".")) {
+        j.innerHTML += getDivHalf(objArr[k].start);
+        j.style.height += `${(objArr[k].end - objArr[k].start) * 120}px`;
+      }
     }
-    
   }
 };
-
-
 
 render();
 collision();
